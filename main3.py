b50ac4a784c241ddf7338543782ea4899063169b
wrednaya33 <shokooth1>
2019-10-01 14:35:28 +0400

    флагшток

---
+++ main3.py
@@ -0,0 +1,16 @@
+import pygame
+
+pygame.init()
+
+scr = pygame.display.set_mode((300, 300))
+scr.fill(pygame.Color('green'))
+
+def draw():
+    pygame.draw.rect(scr, pygame.Color('black'), [(10, 10), (20, 250)], 0)
+    
+draw()
+
+while pygame.event.wait().type != pygame.QUIT:
+    pygame.display.flip()
+
+pygame.quit()
